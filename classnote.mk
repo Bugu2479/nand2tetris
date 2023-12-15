84f83655eb42dbfcc08b6b615aee41a2de237558
Bugu <2624078607@qq.com>
2023-12-15 19:00:52 +0800

    first commit

---
+++ classnote.mk
@@ -0,0 +1,23 @@
+1 门的布尔代数
+ Not: Nand(a,true)
+ And: Nand(a,b) Nand Nand(a,b)
+
+Or:    Nand(a,b) Nand Nand(b,b)
+
+Xor: (a and not b) or (nota and b)
+
+Mux: (nots and a) and (b or not b) or (s and b) and (a or not a)
+
+Dmux: (nots and in) or (s and in)
+
+Not16 : not X 16
+
+And16 : and x 16
+
+Or16:  Or X 16
+
+Or8way: 把数组里面互相or 
+
+Mux16: mux X 16
+
+Mux4way16: 用mux16 两两比较
